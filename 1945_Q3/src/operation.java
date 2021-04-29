041517d42911ae7e12b4541f815f847d727f1b43
SalriaPereira <mca.1945@unigoa.ac.in>
2021-04-29 15:42:21 +0530

    commit first

---
+++ 1945_Q3/src/operation.java
@@ -0,0 +1,20 @@
+public class operation 
+{
+
+	    public static void main(String args[])
+	  {
+	    int first=5, second=2; 
+	    int add, subtract, multiply;
+	    float devide;
+
+	    add = first + second;
+	    subtract = first - second;
+	    multiply = first * second;
+	    devide = (float) first / second;
+
+	    System.out.println("Sum = " + add);
+	    System.out.println("Difference = " + subtract);
+	    System.out.println("Multiplication = " + multiply);
+	    System.out.println("Division = " + devide);
+	  }
+}
